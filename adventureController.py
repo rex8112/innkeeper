59213eedf3fc9a78d397a3d24d58e167e7bec442
Riley Winkler <rex8112@gmail.com>
2019-12-22 03:07:51 -0600

    Rearrange Player and Enemy class
    Now uses a parent class for shared functions, like calculate

--- adventureController.py
+++ adventureController.py
@@ -35,21 +35,19 @@ class PerLevel:
     spellAmp = 0.01
 
 
-class Player:
+class Character:
     baseXP = 100
     xpRate = 0.03
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, ID):
+        self.id = ID
 
-    def new(self, name, cls, race, rawAttributes):
+    def new(self, name, cls, race, rawAttributes, skills, rng): # This should be overridden
         self.name = name
         self.cls = cls
         self.race = race
         self.level = 1
         self.xp = 0
-        self.maxHealth = 100
-        self.available = True
         # Attributes
         self.rawStrength = rawAttributes[0]
         self.rawDexterity = rawAttributes[1]
@@ -58,7 +56,7 @@ class Player:
         self.rawWisdom = rawAttributes[4]
         self.rawCharisma = rawAttributes[5]
         # Skills
-        self.skills = []
+        self.skills = ['attack'] + skills
         # Equipment
         self.mainhand = Equipment(1)
         self.offhand = Equipment(1)
@@ -69,85 +67,6 @@ class Player:
         self.trinket = Equipment(1)
 
         self.inventory = []
-        if db.addAdventurer(self.id, name, cls, race, ','.join(str(e) for e in rawAttributes)):
-            self.rest()
-            self.calculate()
-            self.rest()
-            self.save()
-            logger.info('{}:{} Created Successfully'.format(
-                self.id, self.name))
-            return True
-        else:
-            return False
-
-    def delete(self):
-        db.deleteAdventurer(self.id)
-        logger.warning('{}:{} Deleted'.format(self.id, self.name))
-
-    def load(self, calculate=True):
-        try:
-            raw = db.getAdventurer(self.id)
-            self.name = raw[2]
-            self.cls = raw[3]
-            self.level = raw[4]
-            self.xp = raw[5]
-            self.race = raw[6]
-
-            rawAttributes = raw[7].split(',')  # Get a list of the attributes
-            self.rawStrength = int(rawAttributes[0])
-            self.rawDexterity = int(rawAttributes[1])
-            self.rawConstitution = int(rawAttributes[2])
-            self.rawIntelligence = int(rawAttributes[3])
-            self.rawWisdom = int(rawAttributes[4])
-            self.rawCharisma = int(rawAttributes[5])
-
-            try:
-                self.skills = raw[8].split(',')  # Get a list of skills
-            except AttributeError:
-                self.skills = []
-
-            equipment = raw[9].split(',')  # Get a list of equipped items
-            self.mainhand = Equipment(equipment[0])
-            self.offhand = Equipment(equipment[1])
-            self.helmet = Equipment(equipment[2])
-            self.armor = Equipment(equipment[3])
-            self.gloves = Equipment(equipment[4])
-            self.boots = Equipment(equipment[5])
-            self.trinket = Equipment(equipment[6])
-
-            self.inventory = raw[10].split(',')
-            try:
-                self.inventory.remove('')
-            except:
-                pass
-            self.available = bool(raw[11])
-            self.health = int(raw[12])
-
-            if calculate:
-                self.calculate()
-            logger.debug('{}:{} Loaded Successfully'.format(
-                self.id, self.name))
-            return True
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            logger.error('{} Failed to Load\n{}:{}'.format(
-                self.id, type(self).__name__, exc))
-            return False
-
-    def save(self):
-        rawAttributes = [self.rawStrength, self.rawDexterity, self.rawConstitution, self.rawIntelligence,
-                         self.rawWisdom, self.rawCharisma]  # Bundles Attributes into a string to be stored
-        rawAttributes = ','.join(str(e) for e in rawAttributes)
-        # Does the same for skills, though skills aren't currently used
-        skills = ','.join(self.skills)
-        equipment = ','.join(str(e) for e in [self.mainhand.id, self.offhand.id,
-                                              self.helmet.id, self.armor.id, self.gloves.id, self.boots.id, self.trinket.id])
-        inventory = ','.join(str(e) for e in self.inventory)
-
-        save = [self.id, self.name, self.cls, self.level, int(
-            self.xp), self.race, rawAttributes, skills, equipment, inventory, int(self.available), self.health]
-        logger.debug('{}:{} Saved Successfully'.format(self.id, self.name))
-        return db.saveAdventurer(save)
 
     def equip(self, e: int):
         try:
@@ -267,7 +186,7 @@ class Player:
         else:
             self.level += count
             return True
-
+    
     def calculate(self):
         # Checks Race/Class for attribute changes
         self.strength = self.rawStrength
@@ -366,7 +285,122 @@ class Player:
         self.health = self.maxHealth
 
 
-class Enemy:
+class Player(Character):
+    baseXP = 100
+    xpRate = 0.03
+
+    def __init__(self, id):
+        self.id = id
+
+    def new(self, name, cls, race, rawAttributes):
+        self.name = name
+        self.cls = cls
+        self.race = race
+        self.level = 1
+        self.xp = 0
+        self.maxHealth = 100
+        self.available = True
+        # Attributes
+        self.rawStrength = rawAttributes[0]
+        self.rawDexterity = rawAttributes[1]
+        self.rawConstitution = rawAttributes[2]
+        self.rawIntelligence = rawAttributes[3]
+        self.rawWisdom = rawAttributes[4]
+        self.rawCharisma = rawAttributes[5]
+        # Skills
+        self.skills = ['attack']
+        # Equipment
+        self.mainhand = Equipment(1)
+        self.offhand = Equipment(1)
+        self.helmet = Equipment(1)
+        self.armor = Equipment(1)
+        self.gloves = Equipment(1)
+        self.boots = Equipment(1)
+        self.trinket = Equipment(1)
+
+        self.inventory = []
+        if db.addAdventurer(self.id, name, cls, race, ','.join(str(e) for e in rawAttributes)):
+            self.rest()
+            self.calculate()
+            self.rest()
+            self.save()
+            logger.info('{}:{} Created Successfully'.format(
+                self.id, self.name))
+            return True
+        else:
+            return False
+
+    def delete(self):
+        db.deleteAdventurer(self.id)
+        logger.warning('{}:{} Deleted'.format(self.id, self.name))
+
+    def load(self, calculate=True):
+        try:
+            raw = db.getAdventurer(self.id)
+            self.name = raw[2]
+            self.cls = raw[3]
+            self.level = raw[4]
+            self.xp = raw[5]
+            self.race = raw[6]
+
+            rawAttributes = raw[7].split(',')  # Get a list of the attributes
+            self.rawStrength = int(rawAttributes[0])
+            self.rawDexterity = int(rawAttributes[1])
+            self.rawConstitution = int(rawAttributes[2])
+            self.rawIntelligence = int(rawAttributes[3])
+            self.rawWisdom = int(rawAttributes[4])
+            self.rawCharisma = int(rawAttributes[5])
+
+            try:
+                self.skills = raw[8].split(',')  # Get a list of skills
+            except AttributeError:
+                self.skills = []
+
+            equipment = raw[9].split(',')  # Get a list of equipped items
+            self.mainhand = Equipment(equipment[0])
+            self.offhand = Equipment(equipment[1])
+            self.helmet = Equipment(equipment[2])
+            self.armor = Equipment(equipment[3])
+            self.gloves = Equipment(equipment[4])
+            self.boots = Equipment(equipment[5])
+            self.trinket = Equipment(equipment[6])
+
+            self.inventory = raw[10].split(',')
+            try:
+                self.inventory.remove('')
+            except:
+                pass
+            self.available = bool(raw[11])
+            self.health = int(raw[12])
+
+            if calculate:
+                self.calculate()
+            logger.debug('{}:{} Loaded Successfully'.format(
+                self.id, self.name))
+            return True
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            logger.error('{} Failed to Load\n{}:{}'.format(
+                self.id, type(self).__name__, exc))
+            return False
+
+    def save(self):
+        rawAttributes = [self.rawStrength, self.rawDexterity, self.rawConstitution, self.rawIntelligence,
+                         self.rawWisdom, self.rawCharisma]  # Bundles Attributes into a string to be stored
+        rawAttributes = ','.join(str(e) for e in rawAttributes)
+        # Does the same for skills, though skills aren't currently used
+        skills = ','.join(self.skills)
+        equipment = ','.join(str(e) for e in [self.mainhand.id, self.offhand.id,
+                                              self.helmet.id, self.armor.id, self.gloves.id, self.boots.id, self.trinket.id])
+        inventory = ','.join(str(e) for e in self.inventory)
+
+        save = [self.id, self.name, self.cls, self.level, int(
+            self.xp), self.race, rawAttributes, skills, equipment, inventory, int(self.available), self.health]
+        logger.debug('{}:{} Saved Successfully'.format(self.id, self.name))
+        return db.saveAdventurer(save)
+
+
+class Enemy(Character):
     xpRate = 0.03
     baseXP = 50
 
@@ -387,7 +421,7 @@ class Enemy:
         self.rawWisdom = rawAttributes[4]
         self.rawCharisma = rawAttributes[5]
         # Skills
-        self.skills = skills
+        self.skills = ['attack'] + skills
         # Equipment
         self.mainhand = Equipment(1)
         self.offhand = Equipment(1)
@@ -405,6 +439,11 @@ class Enemy:
     def delete(self):
         db.deleteEnemy(self.id)
         logger.warning('{}:{} Deleted'.format(self.id, self.name))
+
+    def calculate(self):
+        self.health = 0
+        Character.calculate(self)
+        self.rest()
 
     def load(self, calculate=True):
         try:
@@ -459,98 +498,6 @@ class Enemy:
                 self.race, rawAttributes, skills, equipment, inventory]
         logger.debug('{}:{} Saved Successfully'.format(self.id, self.name))
         return db.saveEnemy(save)
-
-    def calculate(self):
-        # Checks Race/Class for attribute changes
-        self.strength = self.rawStrength
-        self.dexterity = self.rawDexterity
-        self.constitution = self.rawConstitution
-        self.intelligence = self.rawIntelligence
-        self.wisdom = self.rawWisdom
-        self.charisma = self.rawCharisma
-        self.maxHealth = 0
-        self.wc = 3
-        self.ac = 4
-        self.dmg = 0
-        self.strdmg = 0
-        self.dexdmg = 0
-        self.attackSpeed = 2  # Turns per attack
-
-        # TIME FOR EQUIPMENT CALCULATIONS
-        for equip in [self.mainhand, self.offhand, self.helmet, self.armor, self.gloves, self.boots, self.trinket]:
-            self.wc += int(equip.mods.get('wc', 0))
-            self.ac += int(equip.mods.get('ac', 0))
-            self.maxHealth += int(equip.mods.get('health', 0))
-
-            self.strength += int(equip.mods.get('strength', 0))
-            self.dexterity += int(equip.mods.get('dexterity', 0))
-            self.constitution += int(equip.mods.get('constitution', 0))
-            self.intelligence += int(equip.mods.get('intelligence', 0))
-            self.wisdom += int(equip.mods.get('wisdom', 0))
-            self.charisma += int(equip.mods.get('charisma', 0))
-
-            self.dmg += int(equip.mods.get('dmg', 0))
-            self.strdmg += int(equip.mods.get('strdmg', 0))
-            self.dexdmg += int(equip.mods.get('dexdmg', 0))
-            self.attackSpeed += int(equip.mods.get('as', 0))
-
-        # Strength Related Stats First
-        self.unarmDamage = float(self.strength) * PerLevel.unarmDamage
-        logger.debug(
-            '{0.name} Unarmed Damage calculated to: {0.unarmDamage}'.format(self))
-
-        self.inventoryCapacity = self.strength // PerLevel.invCap + \
-            (10 - (10 // PerLevel.invCap))
-        logger.debug(
-            '{0.name} Inventory Capacity calculated to: {0.inventoryCapacity}'.format(self))
-
-        # Dexterity related stats second
-        if self.dexterity <= 40:  # Dexterity below 40
-            self.evasion = float(self.dexterity) * PerLevel.evasion
-            self.critChance = float(self.dexterity) * PerLevel.evasion
-
-        elif self.dexterity > 40 and self.dexterity <= 100:  # Dexterity between 40 and 100
-            self.evasion = PerLevel.softCapEvasion * \
-                (float(self.dexterity) - 40.0) + 40.0 * PerLevel.evasion
-            self.critChance = PerLevel.softCapCritChance * \
-                (float(self.dexterity) - 40.0) + 40.0 * PerLevel.critChance
-
-        else:  # Dexterity above 100
-            self.evasion = PerLevel.softCapEvasion * \
-                (100.0 - 40.0) + 40.0 * PerLevel.evasion
-            self.critChance = PerLevel.softCapCritChance * \
-                (100.0 - 40.0) + 40.0 * PerLevel.critChance
-
-        logger.debug(
-            '{0.name} Evasion calculated to: {0.evasion}'.format(self))
-        logger.debug(
-            '{0.name} Crit Chance calculated to: {0.critChance}'.format(self))
-
-        # Constitution related stats third
-        self.maxHealth += PerLevel.health * self.constitution + 100
-        logger.debug(
-            '{0.name} Max health calculated to: {0.maxHealth}'.format(self))
-
-        # Intelligence related stats fourth
-        self.spellAmp = PerLevel.spellAmp * float(self.intelligence)
-        logger.debug(
-            '{0.name} Spell Amp calculated to: {0.spellAmp}'.format(self))
-
-        # Wisdom related stats fifth
-        self.secretChance = 0
-
-        # Charisma related stats sixth
-        self.discount = 0
-
-        # Set values to their maximum
-        self.attackCooldown = self.attackSpeed
-        self.health = self.maxHealth
-
-        self.dmg += self.strdmg * self.strength + self.dexdmg * self.dexterity
-        if self.dmg == 0:
-            self.dmg = self.unarmDamage
-
-        logger.debug('{0.name} Calculation complete'.format(self))
 
 
 class Equipment:
