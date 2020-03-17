fd76c75b45dfdf4e3867524401a852d1f1206002
Riley Winkler <rex8112@gmail.com>
2020-03-16 23:32:12 -0500

    Add set_action_channels command

--- cogs/admin.py
+++ cogs/admin.py
@@ -101,6 +101,42 @@ class Admin(commands.Cog):
                 pass
 
     @commands.command()
+    @commands.has_guild_permissions(manage_channels=True)
+    @commands.guild_only()
+    async def set_action_channels(self, ctx, count: int):
+        raw_server_data = list(db.get_server(ctx.guild.id))
+        guild = ctx.guild
+        category = next(x for x in guild.categories if x.id == raw_server_data[4])
+        action_channels = []
+        for x in raw_server_data[7].split('|'):
+            channel = guild.get_channel(int(x))
+            if channel:
+                action_channels.append(channel)
+        if count <= 0:
+            return
+        change = count - len(action_channels)
+        if change < 0:
+            change *= -1
+            for _ in range(change):
+                await action_channels[-1].delete(reason='{}|Deleted via command'.format(str(ctx.author)))
+                action_channels.pop()
+        elif change > 0:
+            for _ in range(change):
+                c = await category.create_text_channel(name='actions_{}'.format(len(action_channels) + 1))
+                action_channels.append(c)
+        raw_server_data[7] = '|'.join(str(x.id) for x in action_channels)
+        db.update_server(
+            raw_server_data[2],
+            raw_server_data[1],
+            raw_server_data[3],
+            raw_server_data[4],
+            raw_server_data[5],
+            raw_server_data[6],
+            raw_server_data[7]
+        )
+        await ctx.message.add_reaction('✅')
+
+    @commands.command()
     @commands.has_permissions(administrator=True)
     @commands.guild_only()
     async def setup_server(self, ctx):
@@ -112,7 +148,7 @@ class Admin(commands.Cog):
         embed = discord.Embed(title='Setup Progress 1/?', colour=Colour.infoColour, 
             description='For me to work as intended now and in the future I have to work out of a specific channel category. Would you like me to create my own or use a pre-existing one?')
         embed.set_author(name=guild.name, icon_url=guild.icon_url)
-        embed.add_field(name='Options', value='1️⃣: Create your own.\n2️⃣: Use a pre-existing one.')
+        embed.add_field(name='Options', value='1️⃣: Create your own.\n~~2️⃣: Use a pre-existing one.~~')
         try:
             mainMessage = await author.send(embed=embed)
         except discord.Forbidden:
@@ -128,7 +164,20 @@ class Admin(commands.Cog):
             if isinstance(mainMessage.channel, discord.TextChannel):
                 await mainMessage.clear_reactions()
             if str(reaction) == '1️⃣':
-                embed.title = 'Setup Progress 1/1'
+                embed.title = 'Setup Progress 1/2'
+                embed.description = 'How many channels should I make that are designated for bot-related commands? This can be changed later.'
+                embed.set_footer(text='Respond with the number')
+                embed.clear_fields()
+                await mainMessage.edit(embed=embed)
+                try:
+                    message_response = await self.bot.wait_for('message', timeout=180.0, check = lambda message: message.author.id == ctx.author.id and message.channel.id == ctx.author.dm_channel.id)
+                    action_count = int(message_response.content)
+                except asyncio.TimeoutError:
+                    await mainMessage.edit(embed=tout)
+                    return
+                except (ValueError, AttributeError):
+                    action_count = 1
+                embed.title = 'Setup Progress 2/2'
                 embed.description = 'I will now create a new category and channels, you may edit the channels as you see fit but do not delete and recreate them.'
                 embed.clear_fields()
                 await mainMessage.edit(embed=embed)
@@ -146,9 +195,15 @@ class Admin(commands.Cog):
                 generalChannel = await category.create_text_channel(name='innkeepers_bar',
                                                                     reason=reason,
                                                                     topic='Discussion related to The Inn.')
-                commandChannel = await category.create_text_channel(name='actions',
-                                                                    reason=reason,
-                                                                    topic='All commands for The Innkeeper goes here.')
+                commandChannels = []
+                for i in range(action_count):
+                    commandChannels.append(
+                        await category.create_text_channel(
+                            name='actions_{}'.format(i+1),
+                            reason=reason,
+                            topic='All commands for The Innkeeper goes here.')
+                    )
+                commandChannel = commandChannels[0]
             # elif str(reaction) == '2️⃣':
             #     embed.title = 'Setup Progress 2/4'
             #     embed.description = 'I am now going to need the appropriate IDs from you.'
@@ -158,12 +213,12 @@ class Admin(commands.Cog):
             categoryID = category.id
             announcementID = announcementChannel.id
             generalID = generalChannel.id
-            commandID = commandChannel.id
+            commandID = '|'.join(str(x.id) for x in commandChannels)
             db.add_server(guildID, guild.name, author.id, categoryID, announcementID, generalID, commandID)
             await ctx.message.add_reaction('✅')
 
             announceEmbed = discord.Embed(title='Hello Citizens of {}!'.format(guild.name), colour=Colour.infoColour,
-                description='I have arrived to answer a plea for adventure, and a plea for drinks. Both I can offer and both you can have, with a little work that is. You may call me, **The Innkeeper**.\nMy Inn will always be open to everyone, no matter their alignment.\n\n***(Bot is heavily still in early alpha and has a hard limitation at this moment until a few things are reworked. Think of this as more of a preview for what is to come.)***')
+                description='I have arrived to answer a plea for adventure, and a plea for drinks. Both I can offer and both you can have, with a little work that is. You may call me, **The Innkeeper**.\nMy Inn will always be open to everyone, no matter their alignment.')
             announceEmbed.add_field(name='How to get started',
                                     value='To begin your adventure, run the `{}begin` command in {}. You will then be walked through a multi-step process to go from becoming a **citizen** to an **adventurer**.'.format(self.bot.CP, commandChannel.mention))
             await announcementChannel.send(embed=announceEmbed)
@@ -190,8 +245,8 @@ class Admin(commands.Cog):
                 category = tmp.category
                 try:
                     for channel in category.channels:
-                        await channel.delete('Cleaning up my mess before I go')
-                    await category.delete('Cleaning up my mess before I go')
+                        await channel.delete(reason='Cleaning up my mess before I go')
+                    await category.delete(reason='Cleaning up my mess before I go')
                     await ctx.guild.leave()
                     db.del_server(ID)
                 except discord.Forbidden:
