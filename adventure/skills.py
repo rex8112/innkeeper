74b7db3b1d3af63a042f525b67c04e8c2e2d04cb
Riley Winkler <rex8112@gmail.com>
2020-03-31 01:38:03 -0500

    Add TripleStrike and Cleave

--- adventure/skills.py
+++ adventure/skills.py
@@ -41,6 +41,9 @@ class Skill():
         else:
             self.cooldown = self.max_cooldown 
         self.log = ''
+
+    def get_damage(self, dmg: float):
+        return round(random.uniform((dmg * 0.9), (dmg * 1.1)), 2)
     
     def use(self, user, target, targetGroup: list):
         result = False
@@ -57,10 +60,7 @@ class Attack(Skill):
     def use(self, user, target, targetGroup: list):
         if self.cooldown <= 0:
             self.log = ''
-            dmg = float(user.mods.get('dmg', 0))
-            min_dmg = dmg * 0.9
-            max_dmg = dmg * 1.1
-            dmg = round(random.uniform(min_dmg, max_dmg), 2)
+            dmg = self.get_damage(float(user.mods.get('dmg', 0)))
             critChance = float(user.mods.get('critChance'))
             target_ac = target.mods.get('ac', 0)
             user_wc = user.mods.get('wc', 0)
@@ -72,10 +72,9 @@ class Attack(Skill):
                 if random.uniform(0.0, 1.0) <= chanceToHit:
                     if random.uniform(1.0, 100.0) <= critChance:
                         dmg = dmg * 2
-                        self.log = '**{}** crit **{}** for **{}** Damage.'.format(user.name, target.name, dmg)
-                    target.deal_physical_damage(dmg)
-                    logger.debug('Character hit for {} DMG'.format(dmg))
-                    self.log = '**{}** hit **{}** for **{}** Damage.'.format(user.name, target.name, dmg)
+                    dealt_damage = target.deal_physical_damage(dmg)
+                    logger.debug('Character hit for {} DMG'.format(dealt_damage))
+                    self.log = '**{}** hit **{}** for **{}** Damage.'.format(user.name, target.name, dealt_damage)
                     self.cooldown = self.max_cooldown
                 else:  # You miss
                     logger.debug(
@@ -104,10 +103,7 @@ class BackStab(Skill):
     def use(self, user, target, targetGroup: list):
         if self.cooldown <= 0:
             self.log = ''
-            dmg = float(user.mods.get('dmg', 0))
-            min_dmg = dmg * 0.9
-            max_dmg = dmg * 1.1
-            dmg = round(random.uniform(min_dmg, max_dmg), 2)
+            dmg = self.get_damage(float(user.mods.get('dmg', 0)))
             target_ac = target.mods.get('ac', 0)
             user_wc = user.mods.get('wc', 0)
             target_ac *= 0.9
@@ -126,6 +122,113 @@ class BackStab(Skill):
             else:  # You evaded
                 self.log = '**{1}** evaded **{0}**\'s backstab.'.format(user.name, target.name)
                 self.cooldown = self.max_cooldown
+            return self.log, True
+        else:
+            self.log = '**{}** on cooldown for **{}** more turns.'.format(self.name.capitalize(), self.cooldown)
+            return self.log, False
+
+class TripleStrike(Skill):
+    """Unleash three quick strikes that are less likely to hit.
+    
+    `80% WC`
+    `80% Damage`"""
+    name = 'triplestrike'
+    targetable = 2
+    max_cooldown = 4
+    requirements = {
+        'dexterity': Modifier('dexterity', 13)
+    }
+
+    def use(self, user, target, targetGroup: list):
+        if self.cooldown <= 0:
+            self.log = '{} Unleashed three strikes towards {}: '.format(user.name, target.name)
+            raw_dmg = float(user.mods.get('dmg', 0)) * 0.8
+            critChance = float(user.mods.get('critChance', 0))
+            target_ac = target.mods.get('ac', 0)
+            user_wc = user.mods.get('wc', 0)
+            user_wc *= 0.8
+
+            for _ in range(3):
+                dmg = self.get_damage(raw_dmg)
+                chanceToHit = float(1 + (user_wc - target_ac)
+                    ((user_wc + target_ac) * 0.5))
+                if random.uniform(1.0, 100.0) > user.mods.get('evasion', 0):  # Evasion Check
+                    # If random number is lower than the chance to hit, you hit
+                    if random.uniform(0.0, 1.0) <= chanceToHit:
+                        if random.uniform(1.0, 100.0) <= critChance:
+                            dmg = dmg * 2
+                            self.log += '**CRIT** '
+                        dealt_damage = target.deal_physical_damage(dmg)
+                        logger.debug('Character hit for {} DMG'.format(dealt_damage))
+                        self.log += '**{}**, '.format(dealt_damage)
+                        self.cooldown = self.max_cooldown
+                    else:  # You miss
+                        logger.debug(
+                            'Missed with chanceToHit: {:.1%}'.format(chanceToHit))
+                        self.log += '**MISSED**, '
+                else: # Evaded
+                    self.log += '**EVADED**, '
+            return self.log[:-2], True
+        else:
+            self.log = '**{}** on cooldown for **{}** more turns.'.format(self.name.capitalize(), self.cooldown)
+            return self.log, False
+
+class Cleave(Skill):
+    """A wide swinging attack that strikes neighboring targets.
+    
+    `30% Cleave`"""
+    name = 'cleave'
+    targetable = 2
+    max_cooldown = 0
+
+    def use(self, user, target, targetGroup: list):
+        if self.cooldown <= 0:
+            self.log = ''
+            position = targetGroup.index(target)
+            dmg = self.get_damage(float(user.mods.get('dmg', 0)))
+            cleave_damage = dmg * .3
+            cleave1 = None
+            cleave2 = None
+            critChance = float(user.mods.get('critChance'))
+            target_ac = target.mods.get('ac', 0)
+            user_wc = user.mods.get('wc', 0)
+            chanceToHit = float(1 + (user_wc - target_ac) /
+                ((user_wc + target_ac) * 0.5))
+
+            if random.uniform(1.0, 100.0) > user.mods.get('evasion', 0):  # Evasion Check
+                # If random number is lower than the chance to hit, you hit
+                if random.uniform(0.0, 1.0) <= chanceToHit:
+                    if random.uniform(1.0, 100.0) <= critChance:
+                        dmg = dmg * 2
+
+                    try:
+                        cleave1 = targetGroup[position-1].deal_physical_damage(cleave_damage)
+                    except IndexError:
+                        pass
+                    try:
+                        cleave2 = targetGroup[position+1].deal_physical_damage(cleave_damage)
+                    except IndexError:
+                        pass
+
+                    dealt_damage = target.deal_physical_damage(dmg)
+                    logger.debug('Character hit for {} DMG'.format(dealt_damage))
+                    self.log = '**{}** cleaved through **{}** for **{}** damage and hit '.format(user.name, target.name, dealt_damage)
+                    if cleave1 and cleave2:
+                        self.log += '**{}** and **{}** for **{}** and **{}** damage.'.format(targetGroup[position-1].name, targetGroup[position+1].name, cleave1, cleave2)
+                    elif cleave1:
+                        self.log += '**{}** for **{}** damage.'.format(targetGroup[position-1].name, cleave1)
+                    elif cleave2:
+                        self.log += '**{}** for **{}** damage.'.format(targetGroup[position+1].name, cleave2)
+                    else:
+                        self.log += 'no one else.'
+                    self.cooldown = self.max_cooldown
+                else:  # You miss
+                    logger.debug(
+                        'Missed with chanceToHit: {:.1%}'.format(chanceToHit))
+                    self.log = '**{}** missed trying to hit **{}**.'.format(user.name, target.name)
+            else:  # You evaded
+                logger.debug('Player Evaded')
+                self.log = '**{1}** evaded **{0}**\'s backstab.'.format(user.name, target.name)
             return self.log, True
         else:
             self.log = '**{}** on cooldown for **{}** more turns.'.format(self.name.capitalize(), self.cooldown)
