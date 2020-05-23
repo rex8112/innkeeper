5188d98826b18d22ddb0c420fbec5e76f0e3a5c4
Riley Winkler <rex8112@gmail.com>
2020-05-23 15:29:07 -0500

    Add activeColour

--- adventure/colour.py
+++ adventure/colour.py
@@ -4,6 +4,7 @@ class Colour:
     creationColour = discord.Colour(0x00DBEB)
     errorColour = discord.Colour(0xFF0000)
     successColour = discord.Colour(0x0DFF00)
+    activeColour = discord.Colour(0x0DFF00)
     infoColour = discord.Colour(0xFFA41C)
     combatColour = discord.Colour(0xFF0000)
     commonColour = discord.Colour(0xFCFCFC)
