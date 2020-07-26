3397c95ecc8e8185e272644998a008f0219f662a
Riley Winkler <rex8112@gmail.com>
2020-07-26 04:16:51 -0500

    Add storage command

--- cogs/inventory.py
+++ cogs/inventory.py
@@ -140,5 +140,82 @@ class Inventory(commands.Cog):
             logger.warning('Equipping Failed', exc_info=True)
             await ctx.message.add_reaction('⛔')
 
+    @commands.command(aliases=['bank'])
+    async def storage(self, ctx):
+        """View your storage and move stuff in and out."""
+        adv = ac.Player(ctx.author.id)
+        storage = ac.Storage(adv)
+        main_embed = discord.Embed(title='Storage', colour=ac.Colour.activeColour,
+            description=f'Storage {len(storage.inventory)}/{storage.slots}')
+        main_embed.set_author(name=adv.name, icon_url=ctx.author.avatar_url)
+        main_embed.set_footer(text='Follow up message: <examine #/compare # #/retrieve #>')
+
+        for count, e in enumerate(storage.inventory, start=1):
+            main_embed.add_field(name=f'Slot {count}', value=e.getInfo(compare_equipment=adv.get_equipment_from_slot(e.slot)))
+
+        main_message = await ctx.send(embed=main_embed)
+        try:
+            value_message = await self.bot.wait_for('message', timeout=30.0, check=lambda message: message.author.id == ctx.author.id and message.channel.id == ctx.channel.id)
+        except asyncio.TimeoutError:
+            main_embed.set_footer(text='')
+            main_embed.colour = ac.Colour.infoColour
+            await main_message.edit(embed=main_embed)
+            return
+        
+        content = value_message.content.split(' ')
+        delete = False
+        try:
+            if content[0] == 'examine':
+                slot = int(content[1])
+                examined_item = storage.inventory[slot - 1]
+                equipped_item = adv.get_equipment_from_slot(examined_item.slot)
+                main_embed.clear_fields()
+                main_embed.colour = ac.Colour.get_rarity_colour(examined_item.getRarity())
+                main_embed.set_footer(text='')
+
+                main_embed.add_field(name=f'Slot {slot}', value=examined_item.getInfo(compare_equipment=equipped_item))
+                main_embed.add_field(name=f'Equipped {equipped_item.slot}', value=equipped_item.getInfo(compare_equipment=examined_item))
+                await main_message.edit(embed=main_embed)
+                delete = True
+            elif content[0] == 'compare':
+                slot_1 = int(content[1])
+                slot_2 = int(content[2])
+                compare_1 = storage.inventory[slot_1 - 1]
+                compare_2 = storage.inventory[slot_2 - 1]
+                main_embed.clear_fields()
+                main_embed.colour = ac.Colour.get_rarity_colour(compare_1.getRarity())
+                main_embed.set_footer(text='')
+
+                main_embed.add_field(name=f'Slot {slot_1}', value=compare_1.getInfo(compare_equipment=compare_2))
+                main_embed.add_field(name=f'Slot {slot_2}', value=compare_2.getInfo(compare_equipment=compare_1))
+                await main_message.edit(embed=main_embed)
+                delete = True
+            elif content[0] == 'retrieve':
+                slot = int(content[1])
+                if len(adv.inventory) < adv.inventoryCapacity:
+                    item = storage.remove_item(slot-1)
+                    adv.addInv(item)
+                    embed = discord.Embed(title='Success', colour=ac.Colour.successColour, description='Item retrieved.')
+                    await main_message.edit(embed=embed)
+                    adv.save()
+                    storage.save()
+                else:
+                    main_embed.colour = ac.Colour.infoColour
+                    main_embed.set_footer(text='Inventory Full')
+                    await main_message.edit(embed=main_embed)
+                delete = True
+            else:
+                main_embed.colour = ac.Colour.infoColour
+                main_embed.set_footer(text='')
+                await main_message.edit(embed=main_embed)
+            if delete:
+                await value_message.delete()
+
+        except IndexError:
+            main_embed.colour = ac.Colour.infoColour
+            main_embed.set_footer(text='Invalid Slot #')
+            await main_message.edit(embed=main_embed)
+            await value_message.delete()
+
 def setup(bot):
     bot.add_cog(Inventory(bot))
