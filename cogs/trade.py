4dbd9e309c4a765871b0173cb85ebab69ef4b58e
Riley Winkler <rex8112@gmail.com>
2020-08-06 04:33:28 -0500

    Finish trade command

--- cogs/trade.py
+++ cogs/trade.py
@@ -17,12 +17,25 @@ handler2.setFormatter(logging.Formatter(
 logger.addHandler(handler)
 logger.addHandler(handler2)
 
+def is_available():
+    async def predicate(ctx):
+        adv = ac.Player(ctx.author.id, False)
+        adv.load(False)
+        if adv.loaded:
+            return adv.available
+        else:
+            return False
+    return commands.check(predicate)
+
 class Trade(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.group()
+    @is_available()
     async def trade(self, ctx):
+        if ctx.invoked_subcommand is not None:
+            return
         adv = ac.Player(ctx.author.id)
         if not adv.loaded:
             raise ac.InvalidAdventurer('Could not load adventurer')
@@ -30,6 +43,7 @@ class Trade(commands.Cog):
         message = None
         response = None
         stage = 'menu'
+        save = False
         arguments = [None]
         while stage != 'cancel':
             if stage == 'menu': # Display the top menu
@@ -51,7 +65,7 @@ class Trade(commands.Cog):
                         name = f'**{i}.** {trade.player_1.name} -> {trade.player_2.name}'
                         value = f'Waiting on: {trade.waiting_on.name}'
                         menu_embed.add_field(name=name, value=value)
-                menu_embed.set_footer(text='Follow up: new/view <index>')
+                menu_embed.set_footer(text='Follow up: view <index>')
                 if not message:
                     message = await ctx.send(embed=menu_embed)
                 else:
@@ -72,23 +86,29 @@ class Trade(commands.Cog):
                     menu_embed.set_footer(text='Error: Index missing')
                     await message.edit(embed=menu_embed)
                     return
+                except ValueError:
+                    menu_embed.colour = ac.Colour.errorColour
+                    menu_embed.set_footer(text='Error: NaN')
+                    await message.edit(embed=menu_embed)
+                    return
                 menu_embed = discord.Embed(
                     title=f'{trade.player_1.name} -> {trade.player_2.name}',
                     colour=ac.Colour.activeColour,
                     description=f'Waiting on: {trade.waiting_on.name}'
                 )
+                menu_embed.set_author(name=adv.name, icon_url=ctx.author.avatar_url)
                 if trade.inventory_1:
-                    inv1 = ''
+                    inv1 = f'Confirmed: {trade.confirm_1}\n\n'
                     for c, i in enumerate(trade.inventory_1, start=1):
                         inv1 += f'**{c}.** {i.get_name()}\n'
                 else:
-                    inv1 = 'Nothing'
+                    inv1 = f'Confirmed: {trade.confirm_1}\n\nNothing'
                 if trade.inventory_2:
-                    inv2 = ''
+                    inv2 = f'Confirmed: {trade.confirm_2}\n\n'
                     for c, i in enumerate(trade.inventory_2, start=1):
                         inv2 += f'**{c}.** {i.get_name()}\n'
                 else:
-                    inv2 = 'Nothing'
+                    inv2 = f'Confirmed: {trade.confirm_2}\n\nNothing'
 
                 menu_embed.add_field(
                     name=f'{trade.player_1.name}\'s Offer',
@@ -99,7 +119,7 @@ class Trade(commands.Cog):
                     value=inv2
                 )
                 if trade.waiting_on == adv:
-                    menu_embed.set_footer(text='Follow up: add/remove/send/cancel')
+                    menu_embed.set_footer(text='Follow up: add/remove <#>/send/confirm/cancel')
                     allow = True
                 else:
                     menu_embed.set_footer(text='Follow up: cancel')
@@ -118,17 +138,21 @@ class Trade(commands.Cog):
                 except discord.Forbidden:
                     pass
                 if action == 'add' and allow: # Add items to trade
+                    save = True
+                    trade.set_confirm(trade.player_1, False)
+                    trade.set_confirm(trade.player_2, False)
                     escape = False
                     footer = ''
                     while escape == False:
                         items_string = ''
-                        for c, i in enumerate(adv.inventory, start=1):
+                        for c, i in enumerate(trade.waiting_on.inventory, start=1):
                             items_string += f'**{c}.** {i.get_name()}'
                         inv_embed = discord.Embed(
                             title='Add item to trade',
                             colour=ac.Colour.activeColour,
-                            description=f'Respond with index number, like the shop.\n\n{items_string}'
+                            description=f'Respond with index number, like the shop. Use `0` to cancel.\n\n{items_string}'
                         )
+                        inv_embed.set_author(name=adv.name, icon_url=ctx.author.avatar_url)
                         inv_embed.set_footer(text=footer)
                         await message.edit(embed=inv_embed)
                         item_response = await self.bot.wait_for('message', timeout=60.0, check=lambda message: ctx.author == message.author and ctx.message.channel.id == message.channel.id)
@@ -144,20 +168,95 @@ class Trade(commands.Cog):
                         if index == 0:
                             escape = True
                         else:
-                            item = adv.remInv(index-1)
-                            if not trade.add_item(adv, item):
-                                adv.addInv(item)
+                            if not trade.add_item(adv, index-1):
                                 footer = 'Error: Items in trade can not exceed 10'
                             else:
                                 footer = ''
                     continue
                 elif action == 'remove' and allow:
-                    pass
+                    save = True
+                    trade.set_confirm(trade.player_1, False)
+                    trade.set_confirm(trade.player_2, False)
+                    try:
+                        index = int(content[1])
+                    except IndexError:
+                        footer = 'Error: Missing index'
+                        continue
+                    except ValueError:
+                        footer = 'Error: NaN'
+                        continue
+                    item = trade.del_item(adv, index-1)
+                    if not adv.addInv(item):
+                        trade.add_item(adv, item)
+                        footer = 'Error: Inventory full'
+                        continue
+                    continue
                 elif action == 'send' and allow:
-                    pass
+                    save = True
+                    trade.set_confirm(trade.player_1, False)
+                    trade.set_confirm(trade.player_2, False)
+                    sender = trade.waiting_on
+                    trade.toggle_waiting_on()
+                    send_embed = discord.Embed(
+                        title='Trade Updated',
+                        colour=ac.Colour.infoColour,
+                        description=f'Your trade with **{sender.name}** has been updated.'
+                    )
+                    user = self.bot.get_user(trade.waiting_on.id)
+                    if user:
+                        try:
+                            await user.send(embed=send_embed)
+                        except discord.Forbidden:
+                            pass
+                    menu_embed = discord.Embed(
+                        title='Trade Sent',
+                        colour=ac.Colour.successColour
+                    )
+                    menu_embed.set_author(name=adv.name, icon_url=ctx.author.avatar_url)
+                    await message.edit(embed=menu_embed)
+                    stage = 'cancel'
+                    continue
+                elif action == 'confirm' and allow:
+                    save = True
+                    sender = trade.waiting_on
+                    trade.set_confirm(adv, True)
+                    trade.toggle_waiting_on()
+                    if not trade.waiting_on.available:
+                        trade.set_confirm(trade.waiting_on, False)
+                    confirm_embed = discord.Embed(
+                        title='Trade Confirmed',
+                        colour=ac.Colour.infoColour,
+                        description=f'**{sender.name}** has confirmed the trade with you.'
+                    )
+                    user = self.bot.get_user(trade.waiting_on.id)
+                    if user:
+                        try:
+                            await user.send(embed=confirm_embed)
+                        except discord.Forbidden:
+                            pass
+                    if trade.confirm_1 == True and trade.confirm_2 == True:
+                        for i in trade.inventory_1:
+                            if not trade.player_2.addInv(i):
+                                storage = ac.Storage(trade.player_2)
+                                storage.add_item(i, True)
+                        for i in trade.inventory_2:
+                            if not trade.player_1.addInv(i):
+                                storage = ac.Storage(trade.player_1)
+                                storage.add_item(i, True)
+                        trade.active = False
+                    menu_embed = discord.Embed(
+                        title='Trade Confirmed',
+                        colour=ac.Colour.successColour
+                    )
+                    await message.edit(embed=menu_embed)
+                    stage = 'cancel'
+                    continue
                 else:
                     stage = 'cancel'
                     continue
+            else:
+                stage = 'cancel'
+                continue
             try:
                 response = await self.bot.wait_for('message', timeout=60.0, check=lambda message: ctx.author == message.author and ctx.message.channel.id == message.channel.id)
             except asyncio.TimeoutError:
@@ -172,9 +271,46 @@ class Trade(commands.Cog):
         menu_embed.colour = ac.Colour.infoColour
         menu_embed.set_footer(text='')
         await message.edit(embed=menu_embed)
-        trade.save()
-        adv.save()
+        if save:
+            trade.save()
 
+    @trade.command()
+    async def new(self, ctx, member: discord.Member):
+        adv1 = ac.Player(ctx.author.id)
+        adv2 = ac.Player(member.id)
+        if not adv1.loaded:
+            embed = discord.Embed(
+                title='Adventurer not found',
+                colour=ac.Colour.errorColour,
+                description='You do not have an adventurer'
+            )
+            await ctx.send(embed=embed)
+            return
+        if not adv2.loaded:
+            embed = discord.Embed(
+                title='Adventurer not found',
+                colour=ac.Colour.errorColour,
+                description=f'{member.display_name} does not have an adventurer'
+            )
+            await ctx.send(embed=embed)
+            return
+        trade = ac.Trade()
+        trade.new(adv1, adv2)
+        trade.save()
+        embed = discord.Embed(
+            title='Trade created',
+            colour=ac.Colour.successColour,
+            description=f'Use `{self.bot.CP}trade` to interact with it.'
+        )
+        await ctx.send(embed=embed)
+        embed = discord.Embed(
+            title=f'{adv1.name} has created a trade with you.',
+            colour=ac.Colour.infoColour
+        )
+        try:
+            await member.send(embed=embed)
+        except discord.Forbidden:
+            pass
 
 def setup(bot):
     bot.add_cog(Trade(bot))
