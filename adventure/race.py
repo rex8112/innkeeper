a69b9a9f39fd3ade313d82138bf11d926159d9fa
Riley Winkler <rex8112@gmail.com>
2020-08-15 22:31:55 -0500

    Add Races

--- adventure/race.py
+++ adventure/race.py
@@ -1,5 +1,35 @@
 class Race:
+    race_dict = {}
     def __init__(self):
+        self.id = 'nonrace'
         self.name = 'NonRace'
         self.description = 'No Description'
-        self.proficiencies =  []
+        self.passive_effect = ''
+
+human = Race()
+human.id = 'human'
+human.name = 'Human'
+human.description = 'Humans are known for their tenacity.'
+human.passive_effect = 'Debuff Resistance'
+Race.race_dict[human.id] = human
+
+elf = Race()
+elf.id = 'elf'
+elf.name = 'Elf'
+elf.description = 'Elves are a rather nimble race and more likely to hit weakpoints.'
+elf.passive_effect = 'Critical Hit Buff'
+Race.race_dict[elf.id] = elf
+
+dwarf = Race()
+dwarf.id = 'dwarf'
+dwarf.name = 'Dwarf'
+dwarf.description = 'Dwarves have a hearty metabolism for their size.'
+dwarf.passive_effect = 'Health Regen'
+Race.race_dict[dwarf.id] = dwarf
+
+goblin = Race()
+goblin.id = 'goblin'
+goblin.name = 'Goblin'
+goblin.description = 'Goblins are greedy little bastards'
+goblin.passive_effect = 'Essense and Gold gain increase'
+Race.race_dict[goblin.id] = goblin
