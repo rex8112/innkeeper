e337626f054724d1633b03265005bd49ed2767a6
Riley Winkler <rex8112@gmail.com>
2021-08-17 17:23:49 -0500

    Convert test.py to database testing

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-import adventure
+from adventure.database import Database
 
-d = adventure.Dungeon()
-d.build_dungeon()
-d.build_map()
-print('DONE')
+db = Database()
+db.insert_adventurer(123, 'Erika', 'adventurer', 'human', '[1,2,3,4,5]', 5634)
+db.delete_adventurer(123)
