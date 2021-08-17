f7965202f300e6d49fe01976bdbc08120c5ca3d3
Riley Winkler <rex8112@gmail.com>
2021-08-17 17:23:18 -0500

    Disable most modules to prevent exceptions

--- adventure/__init__.py
+++ adventure/__init__.py
@@ -9,27 +9,27 @@ __version__ = '0.2.0'
 
 import logging
 
-from .character_class import CharacterClass
-from .characters import Enemy, Player, RaidBoss, test_players
-from .colour import Colour
-from .data import TestData
-from .database import db
-from .dungeons import Dungeon
-from .encounter import Encounter
-from .equipment import BaseEquipment, Equipment
-from .exceptions import *
-from .events import EnemyEvent
-from .formatting import Formatting
-from .modifiers import EliteModifier, Modifier
-from .quests import Quest
-from .race import Race
-from .raid import Raid
-from .server import Server
-from .shop import Shop
-from .skills import Skill
-from .statusEffects import StatusEffect
-from .storage import Storage
-from .trade import Trade
+# from .character_class import CharacterClass
+# from .characters import Enemy, Player, RaidBoss, test_players
+# from .colour import Colour
+# from .data import TestData
+from .database import Database
+# from .dungeons import Dungeon
+# from .encounter import Encounter
+# from .equipment import BaseEquipment, Equipment
+# from .exceptions import *
+# from .events import EnemyEvent
+# from .formatting import Formatting
+# from .modifiers import EliteModifier, Modifier
+# from .quests import Quest
+# from .race import Race
+# from .raid import Raid
+# from .server import Server
+# from .shop import Shop
+# from .skills import Skill
+# from .statusEffects import StatusEffect
+# from .storage import Storage
+# from .trade import Trade
 
 try:
     from logging import NullHandler
