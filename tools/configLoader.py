55b17ebb6f5024080b4c4924f86cb46b19ef0d66
Riley Winkler <rex8112@gmail.com>
2021-08-17 05:41:39 -0500

    Create mariaDB connection

--- tools/configLoader.py
+++ tools/configLoader.py
@@ -16,9 +16,29 @@ except IOError as e:
   logger.critical('config.ini not found, generating one now. Please fill it in.')
   config['settings']['token'] = ''
   config['settings']['ownerID'] = '180067685986467840'
+  config['db']['host'] = 'localhost'
+  config['db']['port'] = '3306'
+  config['db']['user'] = 'root'
+  config['db']['pass'] = ''
+  config['db']['dbname'] = 'innkeeper'
+  config['blueprint']['host'] = 'fwts.ddns.net'
+  config['blueprint']['port'] = '3316'
+  config['blueprint']['user'] = 'blueprintViewer'
+  config['blueprint']['pass'] = '6IPivu'
+  config['blueprint']['dbname'] = 'innkeeper_static'
   config.write()
   sys.exit()
 
 class settings:
   token = config['settings']['token']
   owner = config['settings']['ownerID'].as_int()
+  dbhost = config['db']['host']
+  dbport = config['db']['port'].as_int()
+  dbname = config['db']['dbname']
+  dbuser = config['db']['user']
+  dbpass = config['db']['pass']
+  blueprinthost = config['blueprint']['host']
+  blueprintport = config['blueprint']['port']
+  blueprintname = config['blueprint']['dbname']
+  blueprintuser = config['blueprint']['user']
+  blueprintpass = config['blueprint']['pass']
