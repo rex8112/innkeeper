025a9bf96ab9797c9d0ede99cf9bcd9a100a4675
Riley Winkler <rex8112@gmail.com>
2021-08-28 06:26:14 -0500

    Initial switch

--- README.md
+++
@@ -1,5 +0,0 @@
-# Ethia Bot [Name WIP]
-An Adventure RPG built into a Discord bot using the discord.py library.
-
-## Getting Started
-[Coming Soon]
---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(token);
